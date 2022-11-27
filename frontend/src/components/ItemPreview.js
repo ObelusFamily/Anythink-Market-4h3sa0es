65f6c6eda6a39f11150e0de2f67ce2173d249ce7
Jenny Zilverberg <jennyrud@gmail.com>
2022-11-27 10:10:25 +0200

    #inc-5312 fixing item image for product catalog

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -19,6 +19,7 @@ const mapDispatchToProps = (dispatch) => ({
 
 const ItemPreview = (props) => {
   const item = props.item;
+  const imageUrl = item.image || "./placeholder.png"
 
   const handleClick = (ev) => {
     ev.preventDefault();
@@ -36,7 +37,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={item.image}
+        src={imageUrl}
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
