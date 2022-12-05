d5b8a4f6335aa83534c634139161b7e2d86589ff
Jenny Zilverberg <jennyrud@gmail.com>
2022-12-05 18:29:40 +0200

    SearchBox implementatio

--- frontend/src/components/Home/SearchBox.js
+++ frontend/src/components/Home/SearchBox.js
@@ -5,35 +5,37 @@ import agent from "../../agent";
 const SearchBox = (props) => {
   const [searchValue, setSearchValue] = useState("");
 
+
+  const onSearchTitle = (event) => {
+    event.preventDefault();
+    let value = event.target.value
+    if (value.length >= 3 || (searchValue.length > value.length)) {
+      props.onSubmit(
+        value,
+        (page) => agent.Items.byTitle(value, page),
+        agent.Items.byTitle(value)
+      );
+    }
+    setSearchValue(value)
+  }
+
   return (
     <div id="search-box" className="flex-fill mr-2">
-      <form
-        className="input-group"
-        onSubmit={(event) => {
-          event.preventDefault();
-          props.onSubmit(
-            searchValue,
-            (page) => agent.Items.byTitle(searchValue, page),
-            agent.Items.byTitle(searchValue)
-          );
-        }}
-      >
-        <fieldset className="flex-fill">
-          <div className="input-group">
-            <input
-              className="form-control form-control-md"
-              type="text"
-              placeholder="What is it that you truly desire?"
-              onChange={(event) => setSearchValue(event.target.value)}
-            />
-            <div className="input-group-prepend">
-              <span className="input-group-text" style={{ background: null }}>
-                <i className="bi bi-search text-secondary"></i>
-              </span>
-            </div>
+      <fieldset className="flex-fill">
+        <div className="input-group">
+          <input
+            className="form-control form-control-md"
+            type="text"
+            placeholder="What is it that you truly desire?"
+            onChange={(event) => onSearchTitle(event)}
+          />
+          <div className="input-group-prepend">
+            <span className="input-group-text" style={{ background: null }}>
+              <i className="bi bi-search text-secondary"></i>
+            </span>
           </div>
-        </fieldset>
-      </form>
+        </div>
+      </fieldset>
     </div>
   );
 };
