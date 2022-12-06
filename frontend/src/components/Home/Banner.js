56da89e8538d28693b5321b19ecf64f5145a6374
Jenny Zilverberg <jennyrud@gmail.com>
2022-12-06 14:55:31 +0200

    click on Get

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,19 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 import SearchBox from "./SearchBox";
 
 const Banner = (props) => {
+  const [isGetClicked, setGetClicked] = useState(false);
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
-        <div className="container d-flex flex-row align-items-center">
+        <div className="container d-flex flex-row align-items-center justify-content-center">
           <span className="mr-2">A place to </span>
-          <span className="mr-2" id="get-part">
+          <span
+            className="mr-2"
+            style={{ cursor: "pointer" }}
+            id="get-part"
+            onClick={() => setGetClicked(true)}
+          >
             {" "}
             get
           </span>
-          <SearchBox />
+          {isGetClicked ? <SearchBox /> : null}
           <span>the cool stuff.</span>
         </div>
       </div>
